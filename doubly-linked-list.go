c555330b92ee4b61d383d05ebf75562b1222238f
Khalid Mohamed <khalid.n.mohamed@outlook.com>
2023-12-31 14:42:52 -0500

    Add forward and backward printing, add InsertAfter method

---
+++ doubly-linked-list.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"fmt"
+)
+
+type List[T comparable] struct {
+	head *Node[T]
+	tail *Node[T]
+}
+
+type Node[T comparable] struct {
+	next *Node[T]
+	prev *Node[T]
+	val T
+}
+
+func (l *List[T]) PushFront(val T) {
+	newNode := &Node[T]{val: val}
+
+	if l.head == nil {
+		l.head = newNode
+		l.tail = newNode
+	} else {
+		newNode.next = l.head
+		l.head.prev = newNode
+		l.head = newNode
+	}
+}
+
+func (l *List[T]) PushBack(val T) {
+	newNode := &Node[T]{val: val}
+
+	if l.tail == nil {
+		l.head = newNode
+		l.tail = newNode
+	} else {
+		l.tail.next = newNode
+		newNode.prev = l.tail
+		l.tail = newNode
+	}
+}
+
+func (l *List[T]) PrintListForward() {
+	curr := l.head
+	for curr != nil {
+		fmt.Printf("%v -> ", curr.val)
+		curr = curr.next
+	}
+	fmt.Println("nil")
+}
+
+func (l *List[T]) PrintListBackwards() {
+	curr := l.tail
+	for curr != nil {
+		fmt.Printf("%v -> ", curr.val)
+		curr = curr.prev
+	}
+	fmt.Println("nil")
+}
+
+//Inserts a node after the value T if it exsts in the list
+func (l *List[T]) InsertAfter(node *Node[T], data T) {
+	curr := l.head
+
+	for curr != nil {
+		next := curr.next
+		if curr.val == data {
+			curr.next = node
+			node.prev = curr
+			node.next = next
+			if next != nil {
+				next.prev = node
+			}
+			break
+		}
+		curr = curr.next
+	}
+}
+
+func main() {
+	l := &List[string]{}
+	l.PushFront("is")
+	l.PushBack("a")
+	l.PushBack("test")
+	l.PushFront("this")
+	l.PrintListForward()
+	l.PushBack("to")
+	l.PushBack("determine")
+	l.PushBack("the")
+	l.PushBack("correct")
+	l.PushBack("order")
+	l.PrintListForward()
+
+	newNode := &Node[string]{val: "print"}
+	l.InsertAfter(newNode, "correct")
+	l.PrintListForward()
+	l.PrintListBackwards()
+}
